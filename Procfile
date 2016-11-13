2937d29cb57dea28391da47f4450fbc3f850a729
Abdul Matin Shah <abdulmateenshah@gmail.com>
2016-11-13 19:57:37 +0500

    Prep for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn rajbro.wsgi --log-file -
---
+++ rajbro/settings/production.py
@@ -0,0 +1,5 @@
+SECRET_KEY = ')7cqk6zno+wyb!q9wu8tfd^-y(6(=p3^117#is@(devj)1h7hz'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
