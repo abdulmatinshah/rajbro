fc717f6554733b27b19b6ca3ae64b40d73aae8b6
Abdul Matin Shah <abdulmateenshah@gmail.com>
2016-11-14 06:43:27 +0500

    changes after recovery from heroku

--- rajbro/settings/base.py
+++ rajbro/settings/base.py
@@ -143,8 +143,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'abdulmateenshah@gmail.com'
-EMAIL_HOST_PASSWORD = 'Plasticsun768'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'abdulmateenshah@gmail.com'
+DEFAULT_FROM_EMAIL = ''
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
