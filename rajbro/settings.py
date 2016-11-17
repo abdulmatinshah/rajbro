101e4f0aff0bc52872c0a2cb2f1a2a8e1bdf528a
Abdul Matin Shah <abdulmateenshah@gmail.com>
2016-11-17 09:04:15 +0500

    Add [*] for heroku

--- rajbro/settings.py
+++ rajbro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')7cqk6zno+wyb!q9wu8tfd^-y(6(=p3^117#is@(devj)1h7hz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
