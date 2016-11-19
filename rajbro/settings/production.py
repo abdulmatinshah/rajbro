b24e030b2a8a15ea46b554f802ec5f3feb3b5112
Abdul Matin Shah <abdulmateenshah@gmail.com>
2016-11-19 09:04:23 +0500

    Add debug false to production.py

--- rajbro/settings/production.py
+++ rajbro/settings/production.py
@@ -1,5 +1,5 @@
 import os
-DEBUG = False
+# DEBUG = False
 
 DATABASES = {
     'default': {
@@ -12,14 +12,14 @@ DATABASES = {
     }
 }
 
-PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+# PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#
+# # Static files (CSS, JavaScript, Images)
+# # https://docs.djangoproject.com/en/1.9/howto/static-files/
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+# STATIC_URL = '/static/'
+#
+# # Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, 'static'),
+# )
